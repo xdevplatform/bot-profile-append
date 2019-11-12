876efb27965cc52f930a73e22b69193070aca4cc
Jessica Garson <jessica.garson@gmail.com>
2019-11-12 10:55:20 -0500

    Create append.py

---
+++ append.py
@@ -0,0 +1,29 @@
+import tweepy
+import yaml
+
+
+def load_data():
+    f = open("secrets.yaml")
+    community = yaml.load(f)
+    process = community["bots"]
+    return process
+
+
+def append_profile(process):
+    for x, y in process.items():
+        auth = tweepy.OAuthHandler(y["CONSUMER_KEY"], y["CONSUMER_SECRET"])
+        auth.set_access_token(y["ACCESS_TOKEN"], y["TOKEN_SECRET"])
+        api = tweepy.API(auth)
+        test = api.lookup_users(user_ids=[y["USER_ID"]])
+        for user in test:
+            print(user.description)
+            api.update_profile(description="{} #TwitterBot".format(user.description))
+
+
+def main():
+    process = load_data()
+    append_profile(process)
+
+
+if __name__ == "__main__":
+    main()
