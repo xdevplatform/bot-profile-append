fa08cc821b357c383b3fcc9b9865fe68ef6ee5e7
Jessica Garson <jessica.garson@gmail.com>
2020-03-16 10:17:02 -0400

    Update append.py

--- append.py
+++ append.py
@@ -6,10 +6,11 @@ def load_data():
     f = open("secrets.yaml")
     community = yaml.safe_load(f)
     process = community["bots"]
-    return process
+    created_by = community["admin"]["handle"]
+    return process, created_by
 
 
-def append_profile(process):
+def append_profile(process, created_by):
     for x, y in process.items():
         auth = tweepy.OAuthHandler(y["CONSUMER_KEY"], y["CONSUMER_SECRET"])
         auth.set_access_token(y["ACCESS_TOKEN"], y["TOKEN_SECRET"])
@@ -17,13 +18,18 @@ def append_profile(process):
         test = api.lookup_users(user_ids=[y["USER_ID"]])
         for user in test:
             print(user.description)
-            api.update_profile(description="{} #TwitterBot".format(user.description))
+            api.update_profile(
+                description="{} #TwitterBot created by @{}".format(
+                    user.description, created_by
+                )
+            )
 
 
 def main():
-    process = load_data()
-    append_profile(process)
+    process, created_by = load_data()
+    append_profile(process, created_by)
 
 
 if __name__ == "__main__":
     main()
+
